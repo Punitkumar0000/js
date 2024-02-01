984d1b86b342c89621abcf44e946b87a75bb2086
Punitkumar0000 <137276994+Punitkumar0000@users.noreply.github.com>
2024-02-01 08:40:36 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "55hdb"
+// let score = null
+// let score = "Punit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33jdh" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Punit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+//"Punit" => true
+ 
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
